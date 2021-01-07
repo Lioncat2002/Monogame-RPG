e0d94bbe1616e9124749f21bc56b36f54dfabf82
lioncatdev <sankhayan2002@gmail.com>
2021-01-07 19:18:01 +0530

    added

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RPG_1
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
